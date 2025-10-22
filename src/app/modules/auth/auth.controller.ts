b4ca7c4890ab08e2f42fa6a9c54108b84eb2b2d4
mdselimme <mdselimdev@gmail.com>
2025-10-22 17:09:14 +0600

    add: set token in cookie

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -3,10 +3,16 @@ import catchAsync from "../../shared/catchAsync";
 import sendResponse from "../../shared/sendResponse";
 import httpStatus from "http-status";
 import { AuthService } from "./auth.service";
+import { setTokenInCookie } from "../../shared/setTokinCookie";
 
 // AUTH LOGIN
 const userLogIn = catchAsync(async (req: Request, res: Response) => {
   const result = await AuthService.authServerLogIn(req.body);
+
+  setTokenInCookie(res, {
+    accessToken: result.accessToken,
+    refreshToken: result.refreshToken,
+  });
 
   sendResponse(res, {
     success: true,
---
+++ src/app/shared/setTokinCookie.ts
@@ -0,0 +1,25 @@
+import { Response } from "express";
+
+interface IToken {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export const setTokenInCookie = (res: Response, token: IToken) => {
+  if (token.accessToken) {
+    res.cookie("accessToken", token.accessToken, {
+      maxAge: 60 * 60 * 24 * 1,
+      secure: true,
+      httpOnly: true,
+      sameSite: "none",
+    });
+  }
+  if (token.refreshToken) {
+    res.cookie("refreshToken", token.accessToken, {
+      maxAge: 60 * 60 * 24 * 30,
+      secure: true,
+      httpOnly: true,
+      sameSite: "none",
+    });
+  }
+};
