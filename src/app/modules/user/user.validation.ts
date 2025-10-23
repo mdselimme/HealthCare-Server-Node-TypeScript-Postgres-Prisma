38cfee4d4b797a5426ae5adef41515558008c2ee
mdselimme <mdselimdev@gmail.com>
2025-10-23 15:13:49 +0600

    add: create doctor zod schema

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,5 +1,6 @@
 import z from "zod";
 
+//CREATE USER WITH PATIENT ZOD SCHEMA
 export const createUserValidationZodSchema = z.object({
   password: z
     .string({
@@ -21,4 +22,24 @@ export const createUserValidationZodSchema = z.object({
       })
       .optional(),
   }),
+});
+
+//CREATE DOCTOR ZOD SCHEMA
+export const createDoctorValidationZodSchema = z.object({
+  name: z.string({ error: "type is string." }).min(3, { error: "min 3 characters need." }),
+  contactNumber: z
+    .string()
+    .length(11, { message: "Phone number must be exactly 11 digits" })
+    .regex(/^01\d{9}$/, {
+      message:
+        "Invalid Bangladeshi phone number. It must start with '01' and be exactly 11 digits long.",
+    }),
+  address: z.string({ error: "address type is string." }),
+  registrationNumber: z.string({ error: "registrationNumber type is string." }),
+  experience: z.number({ error: "experience type is number." }),
+  gender: z.enum(["MALE", "FEMALE"], { error: "Value must be MALE OR FEMALE." }),
+  appointmentFee: z.number({ error: "appointmentFee type is number." }).min(0, { error: "value must be greater than 0." }),
+  qualification: z.string({ error: "qualification type is string." }),
+  currentWorkingPlace: z.string({ error: "current Working Place type is string." }),
+  designation: z.string({ error: "designation type is string." }),
 });
